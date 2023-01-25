8a86879141180e097b03279e1f8d40bd5cdea63a
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-25 22:04:59 +0600

    Add files via upload

---
+++ Vowel/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.Write("\n\n");
+            Console.Write("check whether the input alphabet is a vowel or not:\n"); Console.Write(" 	"); Console.Write("\n\n");
+
+            Console.Write("Input an Alphabet (A-Z or a-z) : ");
+            ch = Convert.ToChar(Console.ReadLine().ToLower()); int i = ch;
+            if (i >= 48 && i <= 57)
+            {
+                Console.Write("You entered a number, Please enter an alphabet.");
+            }
+            else
+            {
+                switch (ch)
+                {
+                    case 'a':
+                        Console.WriteLine("The Alphabet is vowel"); 
+                        break;
+                    case 'i':
+                        Console.WriteLine("The Alphabet is vowel"); 
+                        break;
+                    case 'o':
+                        Console.WriteLine("The Alphabet is vowel"); 
+                        break;
+                    case 'u':
+                        Console.WriteLine("The Alphabet is vowel"); 
+                        break;
+                    case 'e':
+                        Console.WriteLine("The Alphabet is vowel"); 
+                        break;
+                    default:
+                        Console.WriteLine("The Alphabet is a consonant"); 
+                        break;
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
