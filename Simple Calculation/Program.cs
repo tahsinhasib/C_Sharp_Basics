219eb2356488afda087f17a7223cff0cdb92b833
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-25 21:54:57 +0600

    Add files via upload

---
+++ Simple Calculation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2); 
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2); 
+            Console.WriteLine("{0} x {1} = {2}", num1, num2, num1 * num2); 
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2); 
+            Console.WriteLine("{0} mod {1} = {2}", num1, num2, num1 % num2); 
+            Console.ReadKey();
+
+        }
+    }
+}
