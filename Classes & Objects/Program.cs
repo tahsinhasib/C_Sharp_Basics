354e95937e2af27688aefedda7d96e154fa168c8
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-25 22:31:13 +0600

    Add files via upload

---
+++ Classes & Objects/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        public int[] studid = new int[5];
+        public int[] day = new int[5];
+        public int[] month = new int[5];
+        public int[] year = new int[5];
+        public string[] name = new string[5];
+        public string[] cname = new string[5];
+        public void details()
+        {
+            Console.WriteLine("Implementation of Classes and Objects ");
+            Console.WriteLine("**************************************");
+            Console.WriteLine("Enter students details and you can view those details");
+            Console.WriteLine(" 	");
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Student s = new Student();
+                s.details();
+
+                int i;
+                for (i = 0; i < 5; i++)
+                {
+                    Console.Write("Enter Student Id:");
+                    s.studid[i] = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter Student name : ");
+                    s.name[i] = Console.ReadLine();
+                    Console.Write("Enter Course name : ");
+                    s.cname[i] = Console.ReadLine();
+                    Console.Write("Enter date of birth\n Enter day(1-31):");
+                    s.day[i] = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter month(1-12):");
+                    s.month[i] = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter year:");
+                    s.year[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine("\n\nStudent's List\n");
+
+                for (i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("\nStudent ID : " + s.studid[i]);
+                    Console.WriteLine("\nStudent name : " + s.name[i]);
+                    Console.WriteLine("\nCourse name : " + s.cname[i]);
+                    Console.WriteLine("\nDate of birth(dd-mm-yy) : " + s.day[i] + "-" + s.month[i] + "-" + s.year[i]);
+                    Console.ReadKey();
+
+                }
+            }
+        }
+    }
+}
