e2a2842fbcd062ac7d3d8b47fb88ad408344a54c
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-25 22:08:37 +0600

    Add files via upload

---
+++ String Length/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str; /* Declares a string of size 100 */ 
+            int length = 0;
+
+            Console.Write("\n\nFind the length of a string :\n");
+            Console.Write("\n"); 
+            Console.Write("Input the string : ");
+            str = Console.ReadLine();
+
+            foreach (char chr in str)
+            {
+                length += 1;
+            }
+            Console.Write("Length of the string is : {0}\n\n", length); 
+            Console.ReadKey();
+        }
+    }
+}
