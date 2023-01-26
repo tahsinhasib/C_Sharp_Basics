46e7e9102be55bd272ce368ed2ef63073a2a4be4
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-27 00:46:17 +0600

    Add files via upload

---
+++ Variables/Variables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Variables
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 12;
+            int var2 = 13;
+
+            Console.WriteLine("Hello World, This is declaration of variables!");
+            Console.Write("Here you go!! ");
+            Console.WriteLine("Tahsin Hasib");
+            Console.WriteLine("Variable var1 = {0}", var1);
+            Console.WriteLine("Variable var1 = {0}", var2);
+
+            Console.ReadLine();
+        }
+    }
+}
