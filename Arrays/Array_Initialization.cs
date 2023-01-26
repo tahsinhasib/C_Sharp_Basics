a7397f6310ad35d4a0cebf9bc1ffab3f802a8d44
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-27 02:22:41 +0600

    Add files via upload

---
+++ Arrays/Array_Initialization.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] laptop = new string[] { "Dell", "Lenovo", "MacBook" };
+
+            Console.Write("Displaying all the laptops: ");
+            for(int i=0; i<laptop.Length; i++)
+            {
+                Console.Write(laptop[i] + " ");
+            }
+            Console.WriteLine("\n");
+
+            int[] num = new int[] { 4, 5, 6, 7, };
+
+            Console.Write("Displaying all numbers: ");
+            for(int i=0; i<num.Length; i++)
+            {
+                Console.Write(num[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
