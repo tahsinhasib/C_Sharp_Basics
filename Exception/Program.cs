fc5e181fa786f4f6d3c581ac7df8c6ddd256ef2a
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-27 11:25:26 +0600

    Add files via upload

---
+++ Exception/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+                // To catch any exceptions which are likely to occur you use a try catch block.
+                string result = "k";
+				Console.WriteLine(Convert.ToInt32(result) + 10);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+			Console.ReadKey();
+        }
+    }
+}
