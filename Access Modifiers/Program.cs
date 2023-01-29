82a811a86a3c6ee10355325ceb29a059d76d9496
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-29 18:55:57 +0600

    Add files via upload

---
+++ Access Modifiers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This can be accessed from outside too!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ User Input/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+
+            /*In this example, we are getting input from the user using 
+             * Console.ReadLine() method. It returns string. For numeric value, 
+             * you need to convert it into int using Convert.ToInt32() method.
+             */
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("It is even number");
+            }
+            else
+            {
+                Console.WriteLine("It is odd number");
+            }
+            Console.ReadKey();
+        }
+    }
+}
