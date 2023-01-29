1afdd0296d0c3fc60e12186bcf93d598d24683f7
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-29 20:13:06 +0600

    Add files via upload

---
+++ Functions/returnType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class returnType
+    {
+        // User defined function  
+        public string Show(string message)
+        {
+            Console.WriteLine("Inside Show Function");
+            return message;
+        }
+
+        public static void Main(string[] args)
+        {
+            returnType program = new returnType();
+            string message = program.Show("Tahsin");
+            Console.WriteLine("Hello " + message);
+            Console.ReadKey();
+        }
+    }
+}
