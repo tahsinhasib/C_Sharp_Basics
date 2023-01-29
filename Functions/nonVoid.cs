78dcad964ce1c0c74d5189616b03d4ce1c986e3c
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-29 20:11:20 +0600

    Add files via upload

---
+++ Functions/nonVoid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class nonVoid
+    {
+        // User defined function without return type  
+        public void Show(string message)
+        {
+            Console.WriteLine("Hello " + message);
+            // No return statement  
+        }
+
+        public static void Main(string[] args)
+        {
+            nonVoid program = new nonVoid();    // Creating Object  
+            program.Show("Disha");              // Calling Function                    
+            Console.ReadKey();
+        }
+    }
+}
