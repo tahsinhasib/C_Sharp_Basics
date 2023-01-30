49bacff8371cbfa94600309588da1c538b1fc782
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-30 20:27:38 +0600

    Add files via upload

---
+++ Jagged Arrays/jaggedarrayex.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    //initializing the jagged array while declaration!
+    public class jaggedarrayex
+    {
+        public static void Main()
+        {
+            int[][] arr = new int[3][]{
+                        new int[] { 11, 21, 56, 78 },
+                        new int[] { 2, 5, 6, 7, 98, 5 },
+                        new int[] { 2, 5 }};
+
+            // Traverse array elements  
+            for (int i=0; i<arr.Length; i++)
+            {
+                for (int j=0; j<arr[i].Length; j++)
+                {
+                    System.Console.Write(arr[i][j] + " ");
+                }
+                System.Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
