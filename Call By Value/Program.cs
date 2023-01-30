be4b2510ae60944c903628eb5a73ea4eb4e0d216
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-30 18:06:54 +0600

    Add files via upload

---
+++ Call By Value/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        // User defined function  
+        public void Show(int val)
+        {
+            val *= val; // Manipulating value  
+            Console.WriteLine("Value inside the show function " + val);
+            // No return statement  
+        }
+        // Main function, execution entry point of the program  
+        public static void Main(string[] args)
+        {
+            int val = 50;
+            Program program = new Program(); // Creating Object  
+            Console.WriteLine("Value before calling the function " + val);
+            program.Show(val); // Calling Function by passing value            
+            Console.WriteLine("Value after calling the function " + val);
+
+            Console.ReadKey();
+        }
+    }
+}
