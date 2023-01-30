210c7a7e83c749876205ddf0f2d27af6c59047c5
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-30 20:18:15 +0600

    Add files via upload

---
+++ Out Parameter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    /*C# provides out keyword to pass arguments as out-type. It is like reference-type, 
+     * except that it does not require variable to initialize before passing. We must use 
+     * out keyword to pass argument as out-type. It is useful when we want a function to 
+     * return multiple values.
+     */
+
+    public class Program
+    {
+        public void Show(out int val) // Out parameter  
+        {
+            int square = 5;
+            val = square;
+            val *= val; 
+        }
+        
+        static void Main(string[] args)
+        {
+            int val = 50;
+            Program program = new Program();  
+            Console.WriteLine("Value before passing out variable " + val);
+
+            program.Show(out val); // Passing out argument  
+            Console.WriteLine("Value after recieving the out variable " + val);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Out Parameter/multiOut.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    /*C# provides out keyword to pass arguments as out-type. It is like reference-type, 
+     * except that it does not require variable to initialize before passing. We must use 
+     * out keyword to pass argument as out-type. It is useful when we want a function to 
+     * return multiple values.
+     */
+
+    public class multiOut
+    {
+        public void Show(out int a, out int b) // Out parameter  
+        {
+            int square = 5;
+            a = square;
+            b = square;
+  
+            a *= a;
+            b *= b;
+        }
+          
+        static void Main(string[] args)
+        {
+            int val1 = 50, val2 = 100;
+            multiOut program = new multiOut(); // Creating Object  
+            Console.WriteLine("Value before passing \n val1 = " + val1 + " \n val2 = " + val2);
+            program.Show(out val1, out val2); // Passing out argument  
+            Console.WriteLine("Value after passing \n val1 = " + val1 + " \n val2 = " + val2);
+
+            Console.ReadKey();
+        }
+    }
+}
