b9ac8de8f972bed8516784e9a02517928e4f35e5
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-30 19:24:59 +0600

    Add files via upload

---
+++ Call By Reference/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        // User defined function  
+        public void Show(ref int val)
+        {
+            val *= val;                                                     // Manipulating value  
+            Console.WriteLine("Value inside the show function " + val); 
+        }
+         
+        static void Main(string[] args)
+        {
+            int val = 50;
+            Program program = new Program();  
+            Console.WriteLine("Value before calling the function " + val);
+
+            program.Show(ref val);        
+            Console.WriteLine("Value after calling the function " + val);
+
+            Console.ReadKey();
+        }
+    }
+}
