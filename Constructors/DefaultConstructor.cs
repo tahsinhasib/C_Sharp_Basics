ef51c2d49584884d0a6e0d64c5c04cec8e1a1935
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-31 20:29:10 +0600

    Add files via upload

---
+++ Constructors/DefaultConstructor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    
+    public class DefaultConstructor
+    {
+        public DefaultConstructor()
+        {
+            Console.WriteLine("Default Constructor Invoked");
+        }
+    }
+    class TestEmployee
+    {
+        public static void Main(string[] args)
+        {
+            DefaultConstructor e1 = new DefaultConstructor();
+            DefaultConstructor e2 = new DefaultConstructor();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Constructors/ParameterizedConstructor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Employee
+    {
+        public int id;
+        public String name;
+        public float salary;
+        public Employee(int i, String n, float s)
+        {
+            id = i;
+            name = n;
+            salary = s;
+        }
+        public void display()
+        {
+            Console.WriteLine(id + " " + name + " " + salary);
+        }
+    }
+    class TestEmployee
+    {
+        public static void Main(string[] args)
+        {
+            Employee e1 = new Employee(101, "Dog", 890000f);
+            Employee e2 = new Employee(102, "Cat", 490000f);
+            e1.display();
+            e2.display();
+
+            Console.ReadKey();
+        }
+    }
+}
