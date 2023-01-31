f7a183bdf67e31a3735d233516c24e60068b61c8
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-31 20:16:35 +0600

    Add files via upload

---
+++ Command Line Arguments/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    
+    public class Program
+    {
+        /*Arguments that are passed by command line known as command line arguments. 
+         * We can send arguments to the Main method while executing the code. The string 
+         * args variable contains all the values passed from the command line.
+         * In the following example, we are passing command line arguments during 
+         * execution of program.
+         */
+
+        // Main function, execution entry point of the program  
+        static void Main(string[] args)                             // string type parameters  
+        {
+            // Command line arguments  
+            Console.WriteLine("Argument length: " + args.Length);
+            Console.WriteLine("Supplied Arguments are:");
+            foreach (Object obj in args)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
