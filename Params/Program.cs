a9e56df13cd66481ac00fdd931ae5baadbb1bc5e
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-31 18:48:53 +0600

    Add files via upload

---
+++ Params/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    //initializing the jagged array while declaration!
+    public class Program
+    {
+        /*In C#, params is a keyword which is used to specify a parameter that takes variable 
+         * number of arguments. It is useful when we don't know the number of arguments prior. 
+         * Only one params keyword is allowed and no additional parameter is permitted after 
+         * params keyword in a function declaration.
+         */
+
+        public void Show(params int[] val)                   // Params Paramater  
+        {
+            for (int i = 0; i < val.Length; i++)
+            {
+                Console.WriteLine(val[i]);
+            }
+        }
+         
+        static void Main(string[] args)
+        {
+            Program program = new Program();                // Creating Object  
+            program.Show(2, 4, 6, 8, 10, 12, 14);           // Passing arguments of variable length  
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Params/Program2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    //initializing the jagged array while declaration!
+    public class Program2
+    {
+        /*In C#, params is a keyword which is used to specify a parameter that takes variable 
+         * number of arguments. It is useful when we don't know the number of arguments prior. 
+         * Only one params keyword is allowed and no additional parameter is permitted after 
+         * params keyword in a function declaration.
+         */
+
+        
+        public void Show(params object[] items)                 // Params Paramater  
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+        }
+        // Main function, execution entry point of the program  
+        static void Main(string[] args)
+        {
+            Program2 program = new Program2(); // Creating Object  
+            program.Show("Valorant", "Apex", 101, 20.50, "CSGO", 'A'); // Passing arguments of variable length
+                                                                                   
+            Console.ReadKey();
+        }
+
+    }
+}
