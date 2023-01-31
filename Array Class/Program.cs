2ac0123fdf8aa0b04c7f3c2bb1d47f8a1706a4ad
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-31 20:09:39 +0600

    Update Program.cs

--- Array Class/Program.cs
+++ Array Class/Program.cs
@@ -2,7 +2,10 @@
 
 namespace ConsoleApp2
 {
-    
+    /*C# provides an Array class to deal with array related operations. It provides methods for creating, 
+    manipulating, searching, and sorting elements of an array. This class works as the base class for all 
+    arrays in the .NET programming environment.
+    */
     public class Program
     {
         static void Main(string[] args)
