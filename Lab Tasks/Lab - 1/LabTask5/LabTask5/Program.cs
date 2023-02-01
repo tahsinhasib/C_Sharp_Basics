88f9952d96da643f796bca5b9ce256597b4ae67b
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-02-01 21:17:38 +0600

    Add files via upload

---
+++ Lab Tasks/Lab - 1/LabTask5/LabTask5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask5
+{
+    class Program
+    {
+        enum Level
+        {
+            Low,
+            Medium,
+            High
+        }
+        static void Main(string[] args)
+        {
+            Level myVar = Level.Medium;
+            Console.WriteLine(myVar);
+            Console.ReadKey();
+        }
+    }
+}
