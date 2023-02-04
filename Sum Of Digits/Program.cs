3fd289791429e5106ca60c709910b1b0e55fd496
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-02-04 20:35:16 +0600

    Add files via upload

---
+++ Sum Of Digits/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class SumofInt
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Enter the number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            while (n > 0)
+            {
+                sum = sum + (n % 10);
+                n = n / 10;
+            }
+            Console.WriteLine("The summation is: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
