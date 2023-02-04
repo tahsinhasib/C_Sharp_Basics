6881cf3057a80f372939c18ebd5cd3093ea9f8b0
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-02-04 20:35:28 +0600

    Add files via upload

---
+++ Prime Number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class funcexer9
+    {
+        public static bool chkprime(int num)
+        {
+            //int k = 0;
+            for (int i = 2; i < num; i++)
+            {
+                //k++;
+                //Console.WriteLine("Execution number " + k);
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static void Main()
+        {
+            Console.Write("Input a number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (chkprime(n))
+                Console.WriteLine(n + " is a prime number");
+            else
+                Console.WriteLine(n + " is not a prime number");
+            Console.ReadKey();
+        }
+    }
+}
