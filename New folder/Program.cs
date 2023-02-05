0de6af5a0c57429fe1f7d47b4b6dd6ca2c363401
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-02-05 17:22:15 +0600

    Add files via upload

---
+++ New folder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class NewProgram
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("HElo");
+            Console.ReadKey();
+        }
+    }
+}
+
