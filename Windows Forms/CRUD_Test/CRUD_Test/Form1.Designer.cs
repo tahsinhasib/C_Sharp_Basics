7f04a1cf8e59850a99f14e7f702b22936027d352
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-02-17 23:43:58 +0600

    Add files via upload

---
+++ Windows Forms/CRUD_Test/CRUD_Test/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace CRUD_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUserID = new System.Windows.Forms.RichTextBox();
+            this.txtUserName = new System.Windows.Forms.RichTextBox();
+            this.txtMobile = new System.Windows.Forms.RichTextBox();
+            this.txtBlood = new System.Windows.Forms.RichTextBox();
+            this.UserDataRecords = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.UserDataRecords)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(235, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USERNAME :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(270, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "USER ID :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(270, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "MOBILE :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(279, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "BLOOD :";
+            // 
+            // txtUserID
+            // 
+            this.txtUserID.Location = new System.Drawing.Point(392, 50);
+            this.txtUserID.Name = "txtUserID";
+            this.txtUserID.Size = new System.Drawing.Size(401, 26);
+            this.txtUserID.TabIndex = 4;
+            this.txtUserID.Text = "";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(392, 96);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(401, 26);
+            this.txtUserName.TabIndex = 5;
+            this.txtUserName.Text = "";
+            // 
+            // txtMobile
+            // 
+            this.txtMobile.Location = new System.Drawing.Point(392, 142);
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.Size = new System.Drawing.Size(401, 26);
+            this.txtMobile.TabIndex = 6;
+            this.txtMobile.Text = "";
+            // 
+            // txtBlood
+            // 
+            this.txtBlood.Location = new System.Drawing.Point(392, 194);
+            this.txtBlood.Name = "txtBlood";
+            this.txtBlood.Size = new System.Drawing.Size(401, 26);
+            this.txtBlood.TabIndex = 7;
+            this.txtBlood.Text = "";
+            // 
+            // UserDataRecords
+            // 
+            this.UserDataRecords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.UserDataRecords.Location = new System.Drawing.Point(193, 299);
+            this.UserDataRecords.Name = "UserDataRecords";
+            this.UserDataRecords.RowHeadersWidth = 51;
+            this.UserDataRecords.RowTemplate.Height = 24;
+            this.UserDataRecords.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.UserDataRecords.Size = new System.Drawing.Size(600, 260);
+            this.UserDataRecords.TabIndex = 8;
+            this.UserDataRecords.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.UserDataRecords_CellClick);
+            this.UserDataRecords.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.UserDataRecords_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gold;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(847, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 40);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "INSERT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gold;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(847, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 40);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "UPDATE";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gold;
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(847, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 40);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "DELETE";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gold;
+            this.button4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(847, 230);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(166, 40);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "RESET";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Gold;
+            this.button5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(847, 290);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(166, 40);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "REFRESH";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1089, 612);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.UserDataRecords);
+            this.Controls.Add(this.txtBlood);
+            this.Controls.Add(this.txtMobile);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtUserID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Operation Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.UserDataRecords)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox txtUserID;
+        private System.Windows.Forms.RichTextBox txtUserName;
+        private System.Windows.Forms.RichTextBox txtMobile;
+        private System.Windows.Forms.RichTextBox txtBlood;
+        private System.Windows.Forms.DataGridView UserDataRecords;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Windows Forms/CRUD_Test/CRUD_Test/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CRUD_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-TGP1F01;Initial Catalog=CRUD_Test;Integrated Security=True");
+        public int UserID;
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserDataRecords_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetUserRecords();
+        }
+
+        private void GetUserRecords()
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM InfoTable", con);
+            DataTable dt = new DataTable();
+
+            con.Open();
+
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+
+            UserDataRecords.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (IsValid())
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO InfoTable VALUES (@id, @name, @mob, @blood)", con);
+                cmd.CommandType= CommandType.Text;
+                cmd.Parameters.AddWithValue("@id", txtUserID.Text);
+                cmd.Parameters.AddWithValue("@name", txtUserName.Text);
+                cmd.Parameters.AddWithValue("@mob", txtMobile.Text);
+                cmd.Parameters.AddWithValue("@blood", txtBlood.Text);
+
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Succesfully registered!", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                GetUserRecords();
+                ResetFromControls();
+            }
+        }
+
+        private bool IsValid()
+        {
+            if(txtUserName.Text == string.Empty)
+            {
+                MessageBox.Show("Username is required!", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ResetFromControls();
+        }
+
+        private void ResetFromControls()
+        {
+            txtUserID.Clear();
+            txtUserName.Clear();
+            txtMobile.Clear();
+            txtBlood.Clear();
+
+            txtUserID.Focus();
+        }
+
+        private void UserDataRecords_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            UserID = Convert.ToInt32(UserDataRecords.Rows[0].Cells[0].Value);
+            txtUserID.Text = UserDataRecords.SelectedRows[0].Cells[0].Value.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new Form1().ShowDialog();
+
+        }
+    }
+}
